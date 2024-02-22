f0bae64c1bd357169f2e05cad2214347fd5cee0f
eamre <emrealkan.1@outlook.com>
2024-02-22 20:06:14 +0300

    ders 12

---
+++ src/Clients/BlazorWeb/BlazorSozluk.WebApp/Infrastructure/Services/EntryService.cs
@@ -0,0 +1,76 @@
+﻿using BlazorSozluk.Common.Models.Page;
+using BlazorSozluk.Common.Models.Queries;
+using BlazorSozluk.Common.Models.RequestModels;
+using BlazorSozluk.WebApp.Infrastructure.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace BlazorSozluk.WebApp.Infrastructure.Services
+{
+    public class EntryService : IEntryService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EntryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<GetEntriesViewModel>> GetEntries()
+        {
+            var result = await _httpClient.GetFromJsonAsync<List<GetEntriesViewModel>>("/api/entry?TodaysEntries=false&Count=30");
+            return result;
+        }
+
+        public async Task<GetEntryDetailViewModel> GetEntryDetail(Guid entryId)
+        {
+            var result = await _httpClient.GetFromJsonAsync<GetEntryDetailViewModel>($"/api/entry/{entryId}");
+            return result;
+        }
+
+        public async Task<PagedViewModel<GetEntryDetailViewModel>> GetMainPageEntries(int page, int pageSize)
+        {
+            var result = await _httpClient.GetFromJsonAsync<PagedViewModel<GetEntryDetailViewModel>>($"/api/entry/mainpageentries?page={page}&pageSize={pageSize}");
+            return result;
+        }
+
+        public async Task<PagedViewModel<GetUserEntriesDetailViewModel>> GetProfilePageEntries(int page, int pageSize, string userName = null)
+        {
+            var result = await _httpClient.GetFromJsonAsync<PagedViewModel<GetUserEntriesDetailViewModel>>($"/api/entry/UserEntries?userName={userName}&page={page}&pageSize={pageSize}");
+            return result;
+        }
+
+        public async Task<PagedViewModel<GetEntryCommentsViewModel>> GetEntryComments(Guid entryId, int page, int pageSize)
+        {
+            var result = await _httpClient.GetFromJsonAsync<PagedViewModel<GetEntryCommentsViewModel>>($"/api/entry/comments/{entryId}?page={page}&pageSize={pageSize}");
+            return result;
+        }
+
+        public async Task<Guid> CreateEntry(CreateEntryCommand command)
+        {
+            var result = await _httpClient.PostAsJsonAsync("/api/Entry/CreateEntry", command);
+            if (!result.IsSuccessStatusCode)
+                return Guid.Empty;
+
+            var guidStr = await result.Content.ReadAsStringAsync();
+            return new Guid(guidStr.Trim('"'));
+        }
+
+        public async Task<Guid> CreateEntryComment(CreateEntryCommentCommand command)
+        {
+            var res = await _httpClient.PostAsJsonAsync("/api/Entry/CreateEntryComment", command);
+
+            if (!res.IsSuccessStatusCode)
+                return Guid.Empty;
+
+            var guidStr = await res.Content.ReadAsStringAsync();
+
+            return new Guid(guidStr.Trim('"'));
+        }
+
+        public async Task<List<SearchEntryViewModel>> SearchBySubject(string searchText)
+        {
+            var result = await _httpClient.GetFromJsonAsync<List<SearchEntryViewModel>>($"/api/entry/Search?searchText={searchText}");
+            return result;
+        }
+    }
+}
---
+++ src/Clients/BlazorWeb/BlazorSozluk.WebApp/Infrastructure/Services/VoteService.cs
@@ -0,0 +1,63 @@
+﻿using BlazorSozluk.Common.Models;
+using BlazorSozluk.WebApp.Infrastructure.Services.Interfaces;
+
+namespace BlazorSozluk.WebApp.Infrastructure.Services
+{
+    public class VoteService : IVoteService
+    {
+        private readonly HttpClient client;
+
+        public VoteService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task DeleteEntryVote(Guid entryId)
+        {
+            var response = await client.PostAsync($"/api/votes/deleteEntryVote/{entryId}", null);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("DeleteEntryVote error");
+        }
+
+        public async Task DeleteEntryCommentVote(Guid entryCommentId)
+        {
+            var response = await client.PostAsync($"/api/votes/deleteEntryCommentVote/{entryCommentId}", null);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("DeleteEntryCommentVote error");
+        }
+
+        public async Task CreateEntryUpVote(Guid entryId)
+        {
+            await CreateEntryVote(entryId, VoteType.UpVote);
+        }
+
+        public async Task CreateEntryDownVote(Guid entryId)
+        {
+            await CreateEntryVote(entryId, VoteType.DownVote);
+        }
+
+        public async Task CreateEntryCommentUpVote(Guid entryCommentId)
+        {
+            await CreateEntryCommentVote(entryCommentId, VoteType.UpVote);
+        }
+
+        public async Task CreateEntryCommentDownVote(Guid entryCommentId)
+        {
+            await CreateEntryCommentVote(entryCommentId, VoteType.DownVote);
+        }
+
+        private async Task<HttpResponseMessage> CreateEntryVote(Guid entryId, VoteType voteType = VoteType.UpVote)
+        {
+            var response = await client.PostAsync($"/api/votes/entry/{entryId}?voteType={voteType}", null);//body de bir şey göndermedi
+
+            return response;
+        }
+
+        private async Task<HttpResponseMessage> CreateEntryCommentVote(Guid entryCommentId, VoteType voteType = VoteType.UpVote)
+        {
+            var response = await client.PostAsync($"/api/votes/entryComment/{entryCommentId}?voteType={voteType}", null);//body de bir şey göndermedi
+
+            return response;
+        }
+    }
+}
